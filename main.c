e63fafa82835189471c7857e1ef6a958c69c4024
CiMON文 <onoettl@outlook.com>
2026-03-11 00:40:14 -0400

    pause

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
+#include <signal.h>
+#include <setjmp.h>
+#include <limits.h>
+#include "calc.h"
 
 int main(int argc, char* argv[]){
         printf("Hello world");
